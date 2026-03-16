f5c8a8c820cdf7a84c0fdcfa78930284fcbecb4d
matl-geo <thiaslarsen@gmail.com>
2026-03-16 10:52:30 +0100

    Moved all functions into processes/.

--- processing/utils.py
+++ processing/utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-PLUGIN_ROOT = Path(__file__).parent
+PROCESSING_DIR = Path(__file__).parent
+PLUGIN_ROOT = Path(PROCESSING_DIR).parent
 RESOURCES_DIR = PLUGIN_ROOT / "resources"
 
 def get_resource(filename: str) -> str:
